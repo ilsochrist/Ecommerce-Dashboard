6f6f97de5aafd42d7ca04a1fb5d70f9e6c2918ae
Ilso Christ <ilso_1000@hotmail.com>
2022-07-22 12:11:25 -0300

    refactor: refactor App components in file

--- src/App.js
+++ src/App.js
@@ -41,32 +41,32 @@ const App = () => {
             }
         >
           <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
-            Navbar
+            <Navbar />
           </div>
         </div>
 
         <div>
           <Routes>
-            <Route path="/" element="Ecommerce" />
-            <Route path="/eccomerce" element="Ecommerce" />
+            <Route path="/" element={<Ecommerce />} />
+            <Route path="/eccomerce" element={<Ecommerce />} />
 
-            <Route path="/orders" element="Orders" />
-            <Route path="/employees" element="Employees" />
-            <Route path="/costumers" element="Costumers" />
+            <Route path="/orders" element={<Orders />} />
+            <Route path="/employees" element={<Employees />} />
+            <Route path="/costumers" element={<Customers />} />
 
-            <Route path="/kanban" element="Kanban" />
-            <Route path="/editor" element="Editor" />
-            <Route path="/calendar" element="Calendar" />
-            <Route path="/color-picker" element="ColorPicker" />
+            <Route path="/kanban" element={<Kanban />} />
+            <Route path="/editor" element={<Editor />} />
+            <Route path="/calendar" element={<Calendar />} />
+            <Route path="/color-picker" element={<ColorPicker />} />
 
-            <Route path="/line" element="Line" />
-            <Route path="/area" element="Area" />
-            <Route path="/bar" element="Bar" />
-            <Route path="/pie" element="Pie" />
-            <Route path="/financial" element="Financial" />
-            <Route path="/color-mapping" element="ColorMapping" />
-            <Route path="/pyramid" element="Pyramid" />
-            <Route path="/stacked" element="Stacked" />
+            <Route path="/line" element={<Line />} />
+            <Route path="/area" element={<Area />} />
+            <Route path="/bar" element={<Bar />} />
+            <Route path="/pie" element={<Pie />} />
+            <Route path="/financial" element={<Financial />} />
+            <Route path="/color-mapping" element={<ColorMapping />} />
+            <Route path="/pyramid" element={<Pyramid />} />
+            <Route path="/stacked" element={<Stacked />} />
           </Routes>
         </div>
 
